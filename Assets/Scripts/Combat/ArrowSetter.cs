8c22de1976753760c1663aae5cb8116fa9de003f
arke0909 <arke0909@gmail.com>
2025-01-23 22:09:50 +0900

    Connect UI

--- Assets/Scripts/Combat/ArrowSetter.cs
+++ Assets/Scripts/Combat/ArrowSetter.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.Combat
 {
@@ -11,16 +12,18 @@ namespace Assets.Scripts.Combat
         [SerializeField] private Transform arrowHolder;
         [SerializeField] private Transform arrowBack;
 
-        [SerializeField] private List<Sprite> arrowSpriteList;
         [SerializeField] private IntEventChannel setArrowEvent;
+        [SerializeField] private List<Sprite> arrowSpriteList;
 
-        private Dictionary<ArrowType, Sprite> arrowSpriteDict = new Dictionary<ArrowType, Sprite>();
+        private Dictionary<ArrowType, Sprite> _arrowSpriteDict = new Dictionary<ArrowType, Sprite>();
         private readonly int _arrowTypeCnt = Enum.GetValues(typeof(ArrowType)).Length;
 
         public List<ArrowType> arrows = new List<ArrowType>();
         
         private void Awake()
         {
+            InitDictionary();
+
             setArrowEvent.ValueEvent += SetArrows;
         }
 
@@ -29,6 +32,17 @@ namespace Assets.Scripts.Combat
             setArrowEvent.ValueEvent -= SetArrows;
         }
 
+        private void InitDictionary()
+        {
+            for(int i = 0; i < arrowSpriteList.Count; i++)
+            {
+                ArrowType arrowType = (ArrowType)i;
+                Debug.Log(arrowType);
+                Sprite sprite = arrowSpriteList[i];
+
+                _arrowSpriteDict.Add(arrowType, sprite);
+            }
+        }
 
         public void SetArrows(int size)
         {
@@ -36,8 +50,17 @@ namespace Assets.Scripts.Combat
 
             for (int i = 0; i < size; i++)
             {
-                ArrowType arrow = (ArrowType)UnityEngine.Random.Range(0, _arrowTypeCnt);
-                arrows.Add(arrow);
+                if (arrowHolder.childCount == 0) break;
+
+                Transform arrowTrm = arrowHolder.GetChild(0);
+                ArrowType arrowType = (ArrowType)UnityEngine.Random.Range(0, _arrowTypeCnt);
+
+                arrowTrm.GetComponent<Image>().sprite = _arrowSpriteDict[arrowType];
+
+                arrowTrm.SetParent(arrowBack);
+
+                arrows.Add(arrowType);
+                
             }
 
         }
