bf820b8efd7b907f59c4bb1e78313873373fd04e
EJY <arke0909@gmail.com>
2025-01-24 19:47:53 +0900

    Arrow

---
+++ Assets/Scripts/Combat/Arrow.cs
@@ -0,0 +1,40 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Combat
+{
+    public class Arrow : MonoBehaviour
+    {
+        [SerializeField] private float scaleDuration;
+        [SerializeField] private float fadeDuration;
+
+        private Image _image;
+
+        private Vector3 _originScale;
+
+
+        private void Awake()
+        {
+            _image = GetComponent<Image>();
+        }
+
+        private void OnEnable()
+        {
+            if (_image.color.a == 0)
+            {
+                Color color = _image.GetComponent<Image>().color;
+                color.a = 1;
+                _image.color = color;
+            }
+                transform.localScale = _originScale * 1.3f;
+            transform.DOScale(_originScale * 1, scaleDuration);
+        }
+
+
+        public void Close()
+        {
+            _image.DOFade(0, fadeDuration);
+        }
+    }
+}
