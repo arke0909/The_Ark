9c33d102c7034ae8aeee6523fd5ac15b2555d7ce
EJY <arke0909@gmail.com>
2025-01-26 19:11:57 +0900

    Fix InGameData

--- Assets/Scripts/Core/InGameData/InGameData.cs
+++ Assets/Scripts/Core/InGameData/InGameData.cs
@@ -4,10 +4,7 @@ namespace Assets.Scripts.Core
 {
     public class InGameData<T> : ScriptableObject
     {
-        T value;
-
-        public void SetValue(T value)
-         => this.value = value;
+        public T value;
 
         public T GetValue() => value;
     }
