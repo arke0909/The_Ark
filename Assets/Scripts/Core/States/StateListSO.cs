5d47fd271dd08ce4872059fe4a6afff730fce5f6
arke0909 <arke0909@gmail.com>
2025-01-29 16:34:16 +0900

    StateListSO

---
+++ Assets/Scripts/Core/States/StateListSO.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Core.States
+{
+    [CreateAssetMenu(fileName = "StateList", menuName = "SO/States/State")]
+    public class StateListSO : ScriptableObject
+    {
+        public List<StateSO> states;
+        private Dictionary<string, StateSO> _statesDict;
+
+        public StateSO this[string stateName] => _statesDict.GetValueOrDefault(stateName);
+
+        private void OnEnable()
+        {
+            _statesDict = new Dictionary<string, StateSO>();
+
+            foreach (var state in states)
+            {
+                _statesDict.Add(state.name, state);
+            }
+        }
+    }
+}
