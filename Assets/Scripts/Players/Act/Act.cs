3f0b175c8a6caa3d943c7ba9f7f32d22bc30f91f
arke0909 <arke0909@gmail.com>
2025-01-29 18:46:35 +0900

    Fix Act & Attack

--- Assets/Scripts/Players/Act/Act.cs
+++ Assets/Scripts/Players/Act/Act.cs
@@ -1,29 +1,12 @@
 using Assets.Scripts.Core.EventChannel;
-using Assets.Scripts.Core.EventChannel.Events;
-using Assets.Scripts.Entities;
 using UnityEngine;
 
 namespace Assets.Scripts.Players.Act
 {
     public abstract class Act : MonoBehaviour
     {
-        [SerializeField] private GameEventChannel turnChangeChannel = default;
-        [SerializeField] private EntityFinder playerFinder;
-
-        protected Player _player;
-        protected virtual void Awake()
-        {
-            _player = playerFinder.entity as Player;
-        }
+        [SerializeField] protected GameEventChannel turnChangeChannel;
 
         public abstract void OnClick();
-
-        private void TurnEnd()
-        {
-            TurnChangeEvent evt = TurnEvents.TurnChangeEvent;
-            evt.isPlayerTurn = false;
-
-            turnChangeChannel.RaiseEvent(evt);
-        }
     }
 }
--- Assets/Scripts/Players/Act/Attack.cs
+++ Assets/Scripts/Players/Act/Attack.cs
@@ -6,22 +6,16 @@ namespace Assets.Scripts.Players.Act
 {
     public class Attack : Act
     {
-        [SerializeField] GameEventChannel attackChannel;
         [SerializeField] IntEventChannel setArrowChannel;
-        [SerializeField] BoolEventChannel changeIsCheckChannel;
         [SerializeField] int arrowSize = 1;
-        [SerializeField] private Vector2 _areaSize;
         public override void OnClick()
         {
-            changeIsCheckChannel.RaiseEvent(true);
             setArrowChannel.RaiseEvent(arrowSize);
 
-            ChangeAreaSizeEvent evt = CombatEvents.ChangeAreaSizeEvent;
-            evt.size = _areaSize;
+            TurnChangeCallingEvent evt = TurnEvents.TurnChangeCallingEvent;
+            evt.turnState = "INPUT";
 
-            attackChannel.RaiseEvent(evt);
-
-            _player.InputCompo.Battle();
+            turnChangeChannel.RaiseEvent(evt);
         }
     }
 }
