5555815ece1908d5ed58e60fcc5be646b82eb4a6
arke0909 <arke0909@gmail.com>
2025-01-29 16:31:26 +0900

    StateSO

---
+++ Assets/Scripts/Core/States/StateSO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Core.States
+{
+    [CreateAssetMenu(fileName = "State", menuName = "SO/States/State")]
+    public class StateSO : ScriptableObject
+    {
+        public string stateName;
+        public string className;
+    }
+}
