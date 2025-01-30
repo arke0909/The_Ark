38f59413b180715f736072f7509654ede234b1b9
EJY <arke0909@gmail.com>
2025-01-30 21:06:48 +0900

    Fix DamageCalcTurnState

--- Assets/Scripts/Core/States/TurnStates/DamageCalcTurnState.cs
+++ Assets/Scripts/Core/States/TurnStates/DamageCalcTurnState.cs
@@ -1,17 +1,33 @@
 ﻿using Assets.Scripts.Core.EventChannel;
+using Assets.Scripts.Core.EventChannel.Events;
 using Scripts.Core.Manager;
+using System;
+using System.Collections;
+using UnityEngine;
 
 namespace Assets.Scripts.Core.States.TurnStates
 {
     public class DamageCalcTurnState : State
     {
+        private float WAIT_SEC = 1;
+
         public DamageCalcTurnState(TurnManager turnManager, GameEventChannel turnChangeChannel, string stateName) : base(turnManager, turnChangeChannel, stateName)
         {
         }
 
         protected override void TurnChange()
         {
+            turnManager.StartCoroutine(DamageCalcCoroutine());
+        }
 
+        private IEnumerator DamageCalcCoroutine()
+        {
+            yield return new WaitForSeconds(WAIT_SEC);
+
+            TurnChangeEvent evt = TurnEvents.TurnChangeEvent;
+            evt.turnState = "ENEMY";
+
+            turnChangeChannel.RaiseEvent(evt);
         }
     }
 }
