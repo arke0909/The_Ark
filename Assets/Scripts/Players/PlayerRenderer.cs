b1f6aae7e39bceef9241a8574e1b96e7cb709897
arke0909 <arke0909@gmail.com>
2025-02-02 02:49:02 +0900

    PlayerRenderer

---
+++ Assets/Scripts/Players/PlayerRenderer.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Scripts.Entities;
+using UnityEngine;
+
+namespace Assets.Scripts.Players
+{
+    public class PlayerRenderer : EntityRenderer, IPlayerComponent
+    {
+        private Player _player;
+
+        public void Initialize(Player player)
+        {
+            _player = player;
+        }
+
+        public void FadeWithTurn(bool isPlayerTurn)
+        {
+            float alpha = isPlayerTurn ? 0.0f : 1.0f;
+            Color color = _renderer.color;
+            color.a = alpha;
+
+            _renderer.color = color;
+        }
+
+    }
+}
