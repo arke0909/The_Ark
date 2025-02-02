bd4e66aab78297c01e1c4cf7343239a8c823ce27
EJY <arke0909@gmail.com>
2025-02-02 23:13:36 +0900

    PoolListSO

---
+++ Assets/Scripts/Core/Pools/PoolListSO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Core.Pools
+{
+    [CreateAssetMenu(menuName = "SO/Pool/List")]
+    public class PoolListSO : ScriptableObject
+    {
+        public List<PoolItemSO> list;
+    }
+}
