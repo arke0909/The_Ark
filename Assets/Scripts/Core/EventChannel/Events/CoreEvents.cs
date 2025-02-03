2e5d35687c53bdd4f45e3dec7347235c10e2db86
EJY <arke0909@gmail.com>
2025-02-03 20:45:37 +0900

    CoreEvents

---
+++ Assets/Scripts/Core/EventChannel/Events/CoreEvents.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts.Core.Pools;
+
+namespace Assets.Scripts.Core.EventChannel.Events
+{
+    public static class CoreEvents
+    {
+        public static PoolPopEvent PoolPopEvent = new PoolPopEvent();
+        public static PoolPushEvent PoolPushEvent = new PoolPushEvent();
+    }
+
+    public class PoolPopEvent : GameEvent
+    {
+        public string poolName;
+    }
+    public class PoolPushEvent : GameEvent
+    {
+        public IPoolable poolable;
+    }
+}
