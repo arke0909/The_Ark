a0606579008a8b7d9020d2309194e7b173fa2ae9
EJY <arke0909@gmail.com>
2025-02-03 21:09:51 +0900

    Pattern

--- Assets/Scripts/Combat/Patterns/Pattern.cs
+++ Assets/Scripts/Combat/Patterns/Pattern.cs
@@ -1,4 +1,8 @@
 ﻿using Assets.Scripts.Combat.Bullets;
+using Assets.Scripts.Core.EventChannel;
+using Assets.Scripts.Core.EventChannel.Events;
+using Assets.Scripts.Core.Pools;
+using Assets.Scripts.Enemies;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,8 +12,30 @@ namespace Assets.Scripts.Combat.Patterns
     {
         [SerializeField] protected List<Transform> firePosTrm = new List<Transform>();
 
+        protected Enemy _enemy;
+        protected GameEventChannel _poolChannel;
+
         public Vector2 areaSize;
         public Bullet bulletPrefab;
+
+        public void InitPattern(Enemy enemy, GameEventChannel poolChannel)
+        {
+            _enemy = enemy;
+            _poolChannel = poolChannel;
+        }
+
+        public IPoolable Pop(string poolName)
+        {
+            PoolPopEvent evt = CoreEvents.PoolPopEvent;
+            evt.poolName = poolName;
+
+            _poolChannel.RaiseEvent(evt);
+            
+            if(evt.poolable == null) return null;
+
+            return evt.poolable;
+        }
+
 
         public abstract void UsePattern();
     }
