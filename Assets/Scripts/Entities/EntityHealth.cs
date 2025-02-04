dbf1f991b8e70fea0e6aad896f24cdfbdb47c799
EJY <arke0909@gmail.com>
2025-02-04 21:43:09 +0900

    EntityHealth

--- Assets/Scripts/Entities/EntityHealth.cs
+++ Assets/Scripts/Entities/EntityHealth.cs
@@ -1,21 +1,47 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.Entities.Stats;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Assets.Scripts.Entities
 {
-    public class EntityHealth : MonoBehaviour
+    public class EntityHealth : MonoBehaviour, IEntityComponent, IAfterInit
     {
+        [SerializeField] private StatSO hp;
 
-        // Use this for initialization
-        void Start()
+        public UnityEvent OnHit;
+        public UnityEvent OnDead;
+
+        public float maxHealth;
+        private float _currentHealth;
+
+        private Entity _entity;
+        private EntityStatComponent _statCompo;
+
+        #region Init Section
+
+        public void Initialize(Entity entity)
         {
-
+            _entity = entity;
+            _statCompo = _entity.GetCompo<EntityStatComponent>();
         }
 
-        // Update is called once per frame
-        void Update()
+        public void AfterInit()
+        {
+            _currentHealth = maxHealth = _statCompo.GetStat(hp).BaseValue;
+        }
+
+        #endregion
+
+        public void ApplyDamage(float damage)
         {
 
+            if(_entity.IsDead) return;
+            _currentHealth = Mathf.Clamp(_currentHealth - damage, 0, maxHealth);
+
+            OnHit?.Invoke();
+
+            if(_currentHealth <= 0)
+                OnDead?.Invoke();
         }
     }
 }
