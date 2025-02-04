02a1c48090e002a104579d765783c56c0b3a9be1
EJY <arke0909@gmail.com>
2025-02-04 20:36:50 +0900

    StatOverride

---
+++ Assets/Scripts/Entities/Stats/StatOverride.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Entities.Stats
+{
+    [Serializable]
+    public class StatOverride
+    {
+        [SerializeField] private StatSO stat;
+        [SerializeField] private bool isUseOverride;
+        [SerializeField] private float overrideBaseValue;
+
+        public StatOverride(StatSO stat) => this.stat = stat;
+
+        public StatSO CreateStat()
+        {
+            StatSO newStat = stat.Clone() as StatSO;
+
+            if(isUseOverride)
+                newStat.BaseValue = overrideBaseValue;
+
+            return newStat;
+        }
+    }
+}
