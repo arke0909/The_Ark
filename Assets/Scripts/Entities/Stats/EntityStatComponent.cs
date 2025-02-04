8f4f1532c8d39ff0d557436a371b056e04b285aa
EJY <arke0909@gmail.com>
2025-02-04 21:17:14 +0900

    EntityStatComponent

---
+++ Assets/Scripts/Entities/Stats/EntityStatComponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.Entities.Stats
+{
+    public class EntityStatComponent : MonoBehaviour, IEntityComponent
+    {
+        [SerializeField] private StatOverride[] statOverrides;
+        private StatSO[] _stats;
+        
+        public Entity Owner { get; private set; }
+        
+        public void Initialize(Entity entity)
+        {
+            Owner = entity;
+            _stats = statOverrides.Select(x => x.CreateStat()).ToArray();
+        }
+
+        public StatSO GetStat(StatSO stat)
+        {
+            Debug.Assert(stat != null, $"Stats::GetStat-stat is null");
+            return _stats.FirstOrDefault(x => x.statName == stat.statName);
+        }
+
+    }
+}
