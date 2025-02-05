2c4fcf82a5c7ef1409eae67ae68c10f44ac6daf2
EJY <arke0909@gmail.com>
2025-02-05 19:13:01 +0900

    Blink Feedback

---
+++ Assets/Scripts/Feedbacks/BlinkFeedback.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Feedbacks
+{
+    public class BlinkFeedback : Feedback
+    {
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        [SerializeField] private float time;
+        [SerializeField] private int blinkCnt;
+
+        private float _blinkTime;
+
+        private void Awake()
+        {
+            _blinkTime = (time / blinkCnt) * 0.5f;
+        }
+
+        public override void StartFeedback()
+        {
+            FinishFeedback();
+
+            StartCoroutine(BlinkCoroutine());
+        }
+
+        private IEnumerator BlinkCoroutine()
+        {
+            for(int i = 0; i < blinkCnt; i++)
+            {
+                Color color = Color.clear;
+                spriteRenderer.color = color;
+
+                yield return new WaitForSeconds(_blinkTime);
+
+                color = Color.white;
+                spriteRenderer.color = color;
+                yield return new WaitForSeconds(_blinkTime);
+            }
+        }
+
+        public override void FinishFeedback()
+        {
+            StopAllCoroutines();
+        }
+    }
+}
