8911fa35b44102f6828b2e1ee91e108b7e936a58
EJY <arke0909@gmail.com>
2025-02-05 19:12:39 +0900

    Shake Feedback

---
+++ Assets/Scripts/Feedbacks/ShakeFeedback.cs
@@ -0,0 +1,31 @@
+﻿using DG.Tweening;
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Feedbacks
+{
+    public class ShakeFeedback : Feedback
+    {
+        [SerializeField] private Transform enemyTrm;
+        [SerializeField] private float duration;
+        [SerializeField] private float strength;
+
+        private Tween shakeTween;
+
+        public override void StartFeedback()
+        {
+            FinishFeedback();
+
+            enemyTrm.DOShakePosition(duration, new Vector2(strength, 0), 5, 0, true, true);
+        }
+
+        public override void FinishFeedback()
+        {
+            if(shakeTween != null)
+            {
+                shakeTween.Kill();
+                shakeTween = null;
+            }
+        }
+    }
+}
