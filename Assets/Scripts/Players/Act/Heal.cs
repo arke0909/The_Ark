50de0c641ee2b672e2794c481bd572663df80d36
EJY <arke0909@gmail.com>
2025-02-09 19:23:03 +0900

    Heal

---
+++ Assets/Scripts/Players/Act/Heal.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.Core.EventChannel.Events;
+using Assets.Scripts.Entities;
+using UnityEngine;
+
+namespace Assets.Scripts.Players.Act
+{
+    public class Heal : Act
+    {
+        [SerializeField] private EntityFinder playerFinder;
+        [SerializeField] private float healValue;
+
+        private EntityHealth _playerHealth;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            _playerHealth = playerFinder.entity.GetCompo<EntityHealth>();
+        }
+
+        public override void ActEffect()
+        {
+            TurnChangeCallingEvent evt = TurnEvents.TurnChangeCallingEvent;
+            evt.turnState = "HEAL";
+
+            turnChangeChannel.RaiseEvent(evt);
+
+            _playerHealth.ApplyHeal(healValue);
+        }
+    }
+}
