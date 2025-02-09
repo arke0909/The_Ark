4ac52c7d2260eaed8e3bdb16f7f9c735f0c8ae63
EJY <arke0909@gmail.com>
2025-02-09 20:46:49 +0900

    Buff

--- Assets/Scripts/Players/Act/Buff.cs
+++ Assets/Scripts/Players/Act/Buff.cs
@@ -1,13 +1,38 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.Core.EventChannel.Events;
+using Assets.Scripts.Entities.Stats;
 using UnityEngine;
 
 namespace Assets.Scripts.Players.Act
 {
     public class Buff : Act
     {
+        [SerializeField] private StatSO attack;
+        [SerializeField] private StatSO criticalChance;
+        [SerializeField] private float attackUpgradeValue;
+        [SerializeField] private float criticalChanceUpgradeValue;
+
+        private EntityStatComponent entityStat;
+
+        private int buffIndex = 0;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            buffIndex = 0;
+            entityStat = playerFinder.entity.GetCompo<EntityStatComponent>();
+        }
+
         public override void ActEffect()
         {
-            Debug.Log("버프");
+            entityStat.AddModifier(attack, $"{attack.name}{buffIndex}", attackUpgradeValue);
+            entityStat.AddModifier(criticalChance, $"{criticalChance.name}{buffIndex}", criticalChanceUpgradeValue);
+
+            buffIndex++;
+
+            TurnChangeCallingEvent evt = TurnEvents.TurnChangeCallingEvent;
+            evt.turnState = "BUFF";
+
+            turnChangeChannel.RaiseEvent(evt);
         }
     }
 }
