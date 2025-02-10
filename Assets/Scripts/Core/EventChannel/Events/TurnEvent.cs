fb45d93e6c5589638d2cbf721dc999d835aa31f0
EJY <arke0909@gmail.com>
2025-02-10 23:07:00 +0900

    PriorityTurnChangeEvent

--- Assets/Scripts/Core/EventChannel/Events/TurnEvent.cs
+++ Assets/Scripts/Core/EventChannel/Events/TurnEvent.cs
@@ -3,10 +3,16 @@ namespace Assets.Scripts.Core.EventChannel.Events
     public static class TurnEvents
     {
         public static TurnChangeEvent TurnChangeEvent = new TurnChangeEvent();
+        public static PriorityTurnChangeEvent PriorityTurnChangeEvent = new PriorityTurnChangeEvent();
         public static TurnChangeCallingEvent TurnChangeCallingEvent = new TurnChangeCallingEvent();
     }
 
     public class TurnChangeEvent : GameEvent
+    {
+        public string nextTurn;
+    }
+
+    public class PriorityTurnChangeEvent : GameEvent
     {
         public string nextTurn;
     }
