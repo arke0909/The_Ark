83fa749c6dbe273d2409f42aa19423005972dadc
EJY <arke0909@gmail.com>
2025-02-10 23:07:12 +0900

    Fix TurnManager

--- Assets/Scripts/Core/Manager/TurnManager.cs
+++ Assets/Scripts/Core/Manager/TurnManager.cs
@@ -1,5 +1,6 @@
 using Assets.Scripts.Core.EventChannel;
 using Assets.Scripts.Core.EventChannel.Events;
+using System;
 using UnityEngine;
 
 namespace Scripts.Core.Manager
@@ -11,23 +12,39 @@ namespace Scripts.Core.Manager
         private void Awake()
         {
             TurnChangeChannel.AddListner<TurnChangeCallingEvent>(HandleTurnChange);
+            TurnChangeChannel.AddListner<PriorityTurnChangeEvent>(HandlePriorityTurnChange);
         }
+
 
         private void OnDestroy()
         {
             TurnChangeChannel.RemoveListner<TurnChangeCallingEvent>(HandleTurnChange);
+            TurnChangeChannel.RemoveListner<PriorityTurnChangeEvent>(HandlePriorityTurnChange);
         }
 
         private void HandleTurnChange(TurnChangeCallingEvent evt)
         {
             TurnChange(evt.nextTurn);
         }
+        private void HandlePriorityTurnChange(PriorityTurnChangeEvent evt)
+        {
+            PriorityTurnChange(evt.nextTurn);
+        }
 
-        private void TurnChange(string turnState)
+        private void TurnChange(string nextTurn)
         {
 
             TurnChangeEvent evt = TurnEvents.TurnChangeEvent;
-            evt.nextTurn = turnState;
+            evt.nextTurn = nextTurn;
+
+            TurnChangeChannel.RaiseEvent(evt);
+        }
+
+        private void PriorityTurnChange(string nextTurn)
+        {
+
+            PriorityTurnChangeEvent evt = TurnEvents.PriorityTurnChangeEvent;
+            evt.nextTurn = nextTurn;
 
             TurnChangeChannel.RaiseEvent(evt);
         }
