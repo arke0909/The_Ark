bc77d09b749fdf728bea5a52f200572cfdf40f1f
EJY <arke0909@gmail.com>
2025-02-14 16:26:56 +0900

    SoundSO

---
+++ Assets/Scripts/Sound/SoundSO.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Sound
+{
+    public enum AudioType
+    {
+        SFX, BGM
+    }
+
+    [CreateAssetMenu(fileName = "SoundSO", menuName = "SO/Sounds/SoundSO")]
+    public class SoundSO : ScriptableObject
+    {
+        public AudioType audioType;
+        public AudioClip clip;
+        public bool loop = false;
+        public bool randomizePitch = false;
+
+        [Range(0, 1)]
+        public float randomizePitchModifier = 0.1f;
+        [Range(0, 1)]
+        public float volume = 1f;
+        [Range(0, 1)]
+        public float basePitch = 1f;
+    }
+}
