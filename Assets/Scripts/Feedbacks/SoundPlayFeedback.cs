7b134dc6275b0e604a4fa0aaccf387f3da5c7cb4
EJY <arke0909@gmail.com>
2025-02-14 17:41:44 +0900

    SoundPlayerFeedback

---
+++ Assets/Scripts/Feedbacks/SoundPlayFeedback.cs
@@ -0,0 +1,38 @@
+﻿using Assets.Scripts.Core.EventChannel;
+using Assets.Scripts.Core.EventChannel.Events;
+using Assets.Scripts.Core.Pools;
+using Assets.Scripts.Sound;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Assets.Scripts.Feedbacks
+{
+    public class SoundPlayFeedback : Feedback
+    {
+        [SerializeField] private GameEventChannel poolChannel;
+        [SerializeField] private SoundSO soundData;
+
+        public override void StartFeedback()
+        {
+            SoundPlayer soundPlayer = Pop("SoundPlayer") as SoundPlayer;
+            soundPlayer.PlaySound(soundData);
+        }
+
+        public override void FinishFeedback()
+        {
+        }
+
+        public IPoolable Pop(string poolName)
+        {
+            PoolPopEvent evt = CoreEvents.PoolPopEvent;
+            evt.poolName = poolName;
+
+            poolChannel.RaiseEvent(evt);
+
+            if (evt.poolable == null) return null;
+
+            return evt.poolable;
+        }
+    }
+}
