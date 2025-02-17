24ed69e87944dcfe4b85bb895b773f283a96d753
EJY <arke0909@gmail.com>
2025-02-17 22:56:16 +0900

    FadeIn

---
+++ Assets/Scripts/UI/FadeIn.cs
@@ -0,0 +1,40 @@
+﻿using Assets.Scripts.Core.EventChannel;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class FadeIn : MonoBehaviour
+    {
+        [SerializeField] private BoolEventChannel fadeChannel;
+        [SerializeField] private GameEventChannel uiChannel;
+        [SerializeField] private float duration;
+
+        private void Awake()
+        {
+
+            fadeChannel.ValueEvent += Fade;
+        }
+
+        private void OnDestroy()
+        {
+            fadeChannel.ValueEvent -= Fade;
+        }
+
+        private void Fade(bool isFadein)
+        {
+            RectTransform rectTrm = GetComponent<RectTransform>();
+
+            float screenHeight = Screen.height;
+
+            float startValue = isFadein ? screenHeight : 0;
+            float endValue = isFadein ? 0 : -screenHeight;
+
+            rectTrm.localPosition = new Vector2(0, startValue);
+            Vector3 endPos = new Vector2(0, endValue);
+
+            DOTween.To(() => rectTrm.localPosition, pos => rectTrm.localPosition = pos, endPos, duration).SetEase(Ease.Linear)
+                .OnComplete(() => Debug.Log(rectTrm.localPosition));
+        }
+    }
+}
