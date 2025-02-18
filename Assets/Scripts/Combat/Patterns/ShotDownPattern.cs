d06a81787dac707feee8bfc3e09260e2d90d3ff1
EJY <arke0909@gmail.com>
2025-02-18 21:20:49 +0900

    ShotDownPattern

---
+++ Assets/Scripts/Combat/Patterns/ShotDownPattern.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.Combat.Bullets;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Combat.Patterns
+{
+    public class ShotDownPattern : Pattern
+    {
+        public override void UsePattern()
+        {
+            ParticleBullet particleBullet = Pop("ShotToDown") as ParticleBullet;
+
+            particleBullet.Init(_damage, firePosTrm[0].position);
+        }
+    }
+}
