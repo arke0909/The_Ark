c3f643a32c97f3154e7abfe3c6553d984e7f7a24
EJY <arke0909@gmail.com>
2025-02-18 21:20:03 +0900

    Fix ParticleBullet

--- Assets/Scripts/Combat/Bullets/ParticleBullet.cs
+++ Assets/Scripts/Combat/Bullets/ParticleBullet.cs
@@ -1,19 +1,63 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts.Core.EventChannel;
+using Assets.Scripts.Core.EventChannel.Events;
+using Assets.Scripts.Core.Pools;
+using Assets.Scripts.Entities;
+using Assets.Scripts.Players;
+using UnityEngine;
 
 namespace Assets.Scripts.Combat.Bullets
 {
-    public class ParticleBullet : Bullet
+    public class ParticleBullet : MonoBehaviour, IPoolable
     {
+        [SerializeField] private GameEventChannel poolChannel;
+        [SerializeField] private string poolName;
+
         private ParticleSystem _particle;
+        private float _damage;
+
+        public GameObject PoolObject => gameObject;
+
+        public string PoolName => poolName;
 
         private void Awake()
         {
             _particle = GetComponent<ParticleSystem>();
         }
 
+        public void Init(float damage, Vector2 position)
+        {
+            transform.position = position;
+
+            _damage = damage;
+
+            _particle.Play();
+        }
+
         private void OnParticleCollision(GameObject other)
         {
-            Debug.Log($"탄막이 {other.name}에 충돌함!");
+            if(other.TryGetComponent(out Player player))
+            {
+                player.GetCompo<EntityHealth>().ApplyDamage(_damage);
+            }
         }
+
+        private void OnParticleSystemStopped()
+        {
+            Push();
+        }
+
+        public void ResetItem()
+        {
+            _damage = 0;
+        }
+
+        private void Push()
+        {
+            PoolPushEvent evt = CoreEvents.PoolPushEvent;
+            evt.poolable = this;
+
+            poolChannel.RaiseEvent(evt);
+        }
+
     }
 }
