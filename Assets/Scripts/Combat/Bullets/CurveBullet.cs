2bac7278de31d85bc4eb8d850e791e5811a5b3fa
EJY <arke0909@gmail.com>
2025-02-20 20:49:35 +0900

    Fix CurveBullet

--- Assets/Scripts/Combat/Bullets/CurveBullet.cs
+++ Assets/Scripts/Combat/Bullets/CurveBullet.cs
@@ -4,6 +4,7 @@ namespace Assets.Scripts.Combat.Bullets
 {
     public class CurveBullet : Bullet
     {
+        [SerializeField] private float curvePower;
         [SerializeField] private AnimationCurve speedCurve;
 
         private Rigidbody2D _rigidCompo;
@@ -13,9 +14,18 @@ namespace Assets.Scripts.Combat.Bullets
             _rigidCompo = GetComponent<Rigidbody2D>();
         }
 
+        public override void InitBullet(Vector2 position, Vector2 dir, float damage)
+        {
+            _damage = damage;
+
+            transform.position = position;
+
+            curvePower *= Mathf.Abs(dir.x);
+        }
+
         void FixedUpdate()
         {
-            _rigidCompo.linearVelocity = new Vector2(speedCurve.Evaluate(Time.time), Vector2.down.y) * speed;
+            _rigidCompo.linearVelocity = new Vector2(speedCurve.Evaluate(Time.time) * curvePower, Vector2.down.y) * speed;
         }
     }
 }
