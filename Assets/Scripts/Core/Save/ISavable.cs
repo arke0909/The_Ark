86d1094cd01f0241c69b086ebffbab7fd758f3f8
EJY <arke0909@gmail.com>
2025-02-21 19:48:30 +0900

    ISavable

---
+++ Assets/Scripts/Core/Save/ISavable.cs
@@ -0,0 +1,9 @@
+﻿namespace Assets.Scripts.Core.Save
+{
+    internal interface ISavable
+    {
+        SaveIDSO ID { get; }
+        public string GetSaveData();
+        public void RestoreData(string loadedData);
+    }
+}
