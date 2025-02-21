931e0bfe4e7b662102bad439b80c6f7a072c4c84
EJY <arke0909@gmail.com>
2025-02-21 19:47:35 +0900

    SaveIDSO

---
+++ Assets/Scripts/Core/Save/SaveIDSO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Core.Save
+{
+    [CreateAssetMenu(fileName = "SaveIdSO", menuName = "SO/System/SaveId")]
+    public class SaveIDSO : MonoBehaviour
+    {
+        public int saveID;
+        public string saveName;
+    }
+}
