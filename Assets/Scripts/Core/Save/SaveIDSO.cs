4e4cfd450feeccb7a17081169eb051cf4a20bba3
EJY <arke0909@gmail.com>
2025-02-22 19:29:49 +0900

    Fix SaveIDSO

--- Assets/Scripts/Core/Save/SaveIDSO.cs
+++ Assets/Scripts/Core/Save/SaveIDSO.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Assets.Scripts.Core.Save
 {
-    [CreateAssetMenu(fileName = "SaveIdSO", menuName = "SO/System/SaveId")]
-    public class SaveIDSO : MonoBehaviour
+    [CreateAssetMenu(fileName = "SaveIdSO", menuName = "SO/System/SaveId")] 
+    public class SaveIDSO : ScriptableObject
     {
         public int saveID;
         public string saveName;
