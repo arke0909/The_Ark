37e2d153c653ea7f1081e372eb7c4fc8d1410676
EJY <arke0909@gmail.com>
2025-02-23 22:53:15 +0900

    SevenBulletsPattern

---
+++ Assets/Scripts/Combat/Patterns/SevenBulletsPattern.cs
@@ -0,0 +1,39 @@
+﻿using Assets.Scripts.Combat.Bullets;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Combat.Patterns
+{
+    public class SevenBulletsPattern : Pattern
+    {
+        [SerializeField] private int repeatCnt;
+        [SerializeField] private float delay;
+        [SerializeField] private float angle;
+        [SerializeField] private int firstBulletCnt;
+        [SerializeField] private int secondBulletCnt;
+
+        public override void UsePattern()
+        {
+            StartCoroutine(PatternCoroutine());
+        }
+
+        private IEnumerator PatternCoroutine()
+        {
+            
+            for (int i = 0; i < repeatCnt; i++)
+            {
+                SpreadParticleBullet firstBullet = Pop("SpreadBullet") as SpreadParticleBullet;
+                firstBullet.ValueSetting(firstBulletCnt, angle, _player.transform.position);
+                firstBullet.Init(_damage, firePosTrm[0].position);
+
+                yield return new WaitForSeconds(delay);
+
+                SpreadParticleBullet secondullet = Pop("SpreadBullet") as SpreadParticleBullet;
+                secondullet.ValueSetting(secondBulletCnt, angle, _player.transform.position);
+                secondullet.Init(_damage, firePosTrm[0].position);
+
+                yield return new WaitForSeconds(delay);
+            }
+        }
+    }
+}
