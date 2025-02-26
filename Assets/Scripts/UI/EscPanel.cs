4bd286660ac58be30221e44ae3fc71534c610d85
EJY <arke0909@gmail.com>
2025-02-26 23:48:54 +0900

    EscPanel

---
+++ Assets/Scripts/UI/EscPanel.cs
@@ -0,0 +1,57 @@
+﻿using Assets.Scripts.Core.EventChannel;
+using Assets.Scripts.Core.EventChannel.Events;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public enum UIWindowStatus
+    {
+        Closed, Opened
+    }
+
+    public class EscPanel : MonoBehaviour
+    {
+        [SerializeField] private GameEventChannel uiChannel;
+        [SerializeField] private InputReader playerInput;
+        [SerializeField] private CanvasGroup canvasGroup;
+
+        private UIWindowStatus _currentUIStatus;
+
+        private void Awake()
+        {
+            uiChannel.AddListener<UIEvent>(HandleEscUI);
+        }
+
+        private void OnDestroy()
+        {
+            uiChannel.RemoveListener<UIEvent>(HandleEscUI);
+        }
+
+        private void HandleEscUI(UIEvent evt)
+        {
+            if(_currentUIStatus == UIWindowStatus.Opened)
+            {
+                Time.timeScale = 1;
+                playerInput.SetActive(true);
+                SetWindow(false);
+                _currentUIStatus = UIWindowStatus.Closed;
+            }
+            else if (_currentUIStatus == UIWindowStatus.Closed)
+            {
+                Time.timeScale = 0;
+                playerInput.SetActive(false);
+                SetWindow(true);
+                _currentUIStatus = UIWindowStatus.Opened;
+
+            }
+        }
+
+        private void SetWindow(bool isOpen)
+        {
+            float alpha = isOpen ? 1.0f : 0.0f;
+            canvasGroup.alpha = alpha;
+            canvasGroup.blocksRaycasts = isOpen;
+            canvasGroup.interactable = isOpen;
+        }
+    }
+}
