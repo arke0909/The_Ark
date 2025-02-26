624b2f9635a6bdd59038cfd16b00f611a581a130
EJY <arke0909@gmail.com>
2025-02-26 23:49:32 +0900

    UI Manager

---
+++ Assets/Scripts/Core/Manager/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Scripts.Core.EventChannel;
+using Assets.Scripts.Core.EventChannel.Events;
+using UnityEngine;
+
+namespace Assets.Scripts.Core.Manager
+{
+    public class UIManager : MonoBehaviour
+    {
+        [SerializeField] private InputReader playerInput;
+        [SerializeField] private GameEventChannel uiChannel;
+
+        private void Awake()
+        {
+            playerInput.EscapeEvent += HandleEscape;
+        }
+
+        private void OnDestroy()
+        {
+            playerInput.EscapeEvent -= HandleEscape;
+        }
+
+        private void HandleEscape()
+        {
+            uiChannel.RaiseEvent(CoreEvents.UIEvent);
+        }
+    }
+}
