3526af54e0bf4d184aeffc77bf6dad485b68ca30
EJY <arke0909@gmail.com>
2025-02-27 19:57:00 +0900

    SoundSlider

---
+++ Assets/Scripts/UI/SoundSlider.cs
@@ -0,0 +1,33 @@
+﻿using Assets.Scripts.Core.InGameData;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class SoundSlider : MonoBehaviour
+    {
+        [SerializeField] private AudioMixer audioMixer;
+        [SerializeField] private FloatData volumeData;
+        [SerializeField] private string volumeName;
+
+        private Slider _bar;
+
+        private void Awake()
+        {
+            _bar = GetComponentInChildren<Slider>();
+            _bar.value = volumeData.value;
+        }
+
+        private void Start()
+        {
+            audioMixer.SetFloat(volumeName, volumeData.value);
+        }
+
+        public void OnValueChange()
+        {
+            volumeData.value = _bar.value;
+            audioMixer.SetFloat(volumeName, Mathf.Log10(volumeData.value) * 20);
+        }
+    }
+}
