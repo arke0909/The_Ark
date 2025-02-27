bb04641bf744c6162c02fd210790c4d735b612ed
EJY <arke0909@gmail.com>
2025-02-27 17:41:42 +0900

    SceneTransitionManager

---
+++ Assets/Scripts/Core/Manager/SceneTransitionManager.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Scripts.Core.EventChannel;
+using Assets.Scripts.Core.EventChannel.Events;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace Assets.Scripts.Core.Manager
+{
+    public class SceneTransitionManager : MonoBehaviour
+    {
+        [SerializeField] private GameEventChannel sceneChannel;
+
+        private void Awake()
+        {
+            sceneChannel.AddListener<SceneEvent>(HandleSceneEvent);
+        }
+
+        private void OnDestroy()
+        {
+            sceneChannel.RemoveListener<SceneEvent>(HandleSceneEvent);
+        }
+
+        private void HandleSceneEvent(SceneEvent evt)
+        {
+            SceneManager.LoadScene(evt.sceneName);
+        }
+    }
+}
