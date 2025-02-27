d587a8a540cf3634ae4e92ad6fbbb06c893307bb
EJY <arke0909@gmail.com>
2025-02-27 22:01:01 +0900

    SceneManagerSO

---
+++ Assets/Scripts/Core/SceneManagerSO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Core
+{
+    [CreateAssetMenu(fileName = "SceneManagerSO", menuName = "SO/System/SceneManagerSO")]
+    public class SceneManagerSO : ScriptableObject
+    {
+        public string GetCurrentSceneName() => SceneManager.GetActiveScene().name;
+
+        public void LoadScene(string sceneName) => SceneManager.LoadScene(sceneName);
+    }
+}
