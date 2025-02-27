9c335b2b81370f0635390d32e02ff9a560512370
EJY <arke0909@gmail.com>
2025-02-27 22:48:37 +0900

    DeadActSelector

---
+++ Assets/Scripts/Players/Act/DeadActSelector.cs
@@ -0,0 +1,21 @@
+﻿namespace Assets.Scripts.Players.Act
+{
+    public class DeadActSelector : ActSelector
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+
+            HandleValueChange(false);
+        }
+
+        protected override void HandleValueChange(bool value)
+        {
+            base.HandleValueChange(value);
+
+            _canvasGroup.alpha = value ? 1 : 0;
+            _canvasGroup.interactable = value;
+            _canvasGroup.blocksRaycasts = value;
+        }
+    }
+}
