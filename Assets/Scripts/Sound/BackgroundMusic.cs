0bf25f39facbfe3c01ca095400abf0f374917968
arke0909 <arke0909@gmail.com>
2025-02-28 18:34:44 +0900

    BackgroundMusic

---
+++ Assets/Scripts/Sound/BackgroundMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Sound
+{
+    public class BackgroundMusic : MonoBehaviour
+    {
+        [SerializeField] private SoundSO soundSO;
+        private AudioSource _audioSource;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+
+            _audioSource.resource = soundSO.clip;
+
+            _audioSource.Play();
+        }
+    }
+}
