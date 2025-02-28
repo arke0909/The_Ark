9af8719831ac2821997101f0968506387f89e6c8
arke0909 <arke0909@gmail.com>
2025-02-28 18:33:30 +0900

    MainTitleCanvas

---
+++ Assets/Scripts/UI/MainTitleCanvas.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.Core;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class MainTitleCanvas : MonoBehaviour
+    {
+        [SerializeField] private SceneManagerSO sceneManager;
+        [SerializeField] private float duration;
+
+        private CanvasGroup _canvasGroup;
+
+        private void Awake()
+        {
+            _canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        public void OnStart()
+        {
+            DOTween.To(() => _canvasGroup.alpha, alpha => _canvasGroup.alpha = alpha, 0, duration)
+                .OnComplete(() => { sceneManager.LoadScene("Stage1"); });
+        }
+
+        public void OnExit()
+        {
+            Application.Quit();
+        }
+    }
+}
