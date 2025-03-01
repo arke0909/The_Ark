9f7c6065b781aaec2b859fdda4de67bc0f6c0abb
EJY <arke0909@gmail.com>
2025-03-01 23:05:42 +0900

    Save Prograss

---
+++ Assets/Scripts/Feedbacks/CameraShakeFeedback.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Feedbacks
+{
+    public class CameraShakeFeedback : Feedback
+    {
+        public override void FinishFeedback()
+        {
+        }
+
+        public override void StartFeedback()
+        {
+        }
+    }
+}
