11a9a31e16711cd5aed3b3c80477f88f4cc3620b
EJY <arke0909@gmail.com>
2025-03-02 18:26:08 +0900

    Camera Manager

---
+++ Assets/Scripts/Core/Manager/CameraManager.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.Core.EventChannel;
+using Assets.Scripts.Core.EventChannel.Events;
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace Assets.Scripts.Core.Manager
+{
+    public class CameraManager : MonoBehaviour
+    {
+        [SerializeField] private GameEventChannel cameraChannel;
+
+        private CinemachineImpulseSource _impulseSource;
+
+        private void Awake()
+        {
+            _impulseSource = GetComponent<CinemachineImpulseSource>();
+            cameraChannel.AddListener<CameraShakeEvent>(HandleCameraShakeEvent);
+        }
+
+        private void OnDestroy()
+        {
+            cameraChannel.RemoveListener<CameraShakeEvent>(HandleCameraShakeEvent);
+        }
+
+        private void HandleCameraShakeEvent(CameraShakeEvent evt)
+        {
+            _impulseSource.GenerateImpulse(evt.shakePower);
+        }
+    }
+}
